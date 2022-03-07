2e1709509c7176b956e7d03aac850c29340e457d
Saptrshi Ghosh <1827052@kiit.ac.in>
2022-03-07 10:26:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,177 @@
+// comment
+
+//let count=0 // count is the variable name
+//console.log(count) // to check out variable output
+
+
+//     Excercise ////
+
+//let myAge=22
+//console.log(myAge) //debugging purposes // ctrl+shift+j in web browser to open console
+
+
+
+
+//let dog = 5+7
+//console.log(dog)
+
+//let cat = 5-7
+//console.log(cat)
+
+//let mouse = 5/7
+//console.log(mouse)
+
+//let tiger = 5*7
+//console.log(tiger)
+
+
+//     Excercise ////
+
+// let firstBatch = 5
+// let secondBatch = 7
+
+// let count = firstBatch + secondBatch
+
+// console.log(count)
+
+
+
+
+//     Excercise ////
+
+//let myAge=22
+//let humanDogRatio= 7
+//let myDogAge = myAge*humanDogRatio
+//console.log(myDogAge)
+
+
+//let count =5
+//count=3 // to change the variable value
+//count=1 // again the value of count changed
+//count = count + 1
+//console.log(count)
+
+
+//Excercise ///
+
+// Ctrl+K+C
+
+// bonusPoints=50
+// console.log(bonusPoints)
+
+// bonusPoints=bonusPoints+50
+// console.log(bonusPoints)
+
+// bonusPoints=bonusPoints-75
+// console.log(bonusPoints)
+
+// bonusPoints=bonusPoints+45
+// console.log(bonusPoints)
+
+
+
+
+
+
+
+
+
+
+//   Pseudo  CODE   //
+
+// intialize the count as 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
+// document.getElementById("count-el").innerText = 5
+// // {document.getElementById("count-el").} asking for the HTML element
+   // {innerText = 5} Modify the HTML elemment
+// change the count-el in the HTML to reflect the new count
+
+
+//camel Case variable naming style
+let countEl=document.getElementById("count-el")
+
+console.log(countEl)
+
+let count=0
+
+function increment(){
+    
+    count=count+1
+    countEl.innerText = count
+}
+
+//increment()
+//increment()
+//increment()
+
+
+/*
+
+ // Excercise : {Car Race} {Making and Calling a function} //
+
+function countdown() {
+    console.log(5)
+    console.log(4)
+    console.log(3)
+    console.log(2)
+    console.log(1)
+}
+
+countndown()
+
+
+// Excercise to create and call a function
+
+function dog(){
+    console.log(42)
+}
+
+dog()
+
+
+*/
+
+
+/*
+
+// Advanced Race Car Game Excercise
+ 
+let lap1 = 34
+let lap2 = 33
+let lap3 = 36
+
+// Create a function that logs out the sum of all the lap times
+
+//console.log(dog)
+
+function cat(){
+    dog = lap1+lap2+lap3
+    console.log(dog)
+}
+
+cat()
+*/
+
+
+/*
+
+
+let lapsCompleted = 0
+
+// Create a function that increments the lapsCompleted variable with one
+// Run it three times
+
+function laps(){
+    lapsCompleted=lapsCompleted+1
+    console.log(lapsCompleted)
+}
+
+
+laps()
+laps()
+laps()
+
+
+
+*/
