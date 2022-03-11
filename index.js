47bd3c030c8a87984aec6e2f99fe61467adf3a51
Saptrshi Ghosh <saptrshighosh22@gmail.com>
2022-03-12 00:20:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-// The Function 
-
 let countEl=document.getElementById("count-el")
-
 console.log(countEl)
 
 let count=0
-
 function increment(){
-    
-    count=count+1
-    countEl.innerText = count
+    count+=1
+    countEl.textContent = count
+}
+
+let saveEl=document.getElementById("save-el")
+console.log(saveEl)
+
+testing=0
+function save(){
+  testing=count + " - "
+  saveEl.textContent+=testing
+
+  countEl.textContent = 0
+  count = 0 
+  
+}
+
+function reset(){
+  countEl.textContent = 0
+  saveEl.textContent = 0
 }
